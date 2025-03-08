e367a5a8d5f4a58477c32d3fe5f277167819a7ba
juanfranbrv <juanfranbrv@gmail.com>
2025-03-08 18:40:53 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# downtube
+# DownTube
+
+DownTube es una aplicación en Python para descargar videos de YouTube.
+
+## Clonar el repositorio
+
+Para clonar el repositorio, usa el siguiente comando:
+
+```bash
+git clone https://github.com/tu-usuario/downtube.git
+```
+
+## Actualizar el repositorio
+
+Para actualizar el repositorio con los últimos cambios, usa el siguiente comando:
+
+```bash
+git pull origin main
+```
+
+## Instalación de dependencias
+
+Para instalar las dependencias necesarias, ejecuta el siguiente comando:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Uso
+
+Para usar la aplicación, ejecuta el siguiente comando:
+
+```bash
+python downtube.py
+```
+
+## Contribuir
+
+Si deseas contribuir a este proyecto, por favor sigue los siguientes pasos:
+
+1. Haz un fork del repositorio.
+2. Crea una nueva rama (`git checkout -b feature/nueva-funcionalidad`).
+3. Realiza tus cambios y haz commit (`git commit -am 'Añadir nueva funcionalidad'`).
+4. Sube tus cambios a tu repositorio (`git push origin feature/nueva-funcionalidad`).
+5. Abre un Pull Request.
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Consulta el archivo `LICENSE` para más detalles.
--- downtube.py
+++ downtube.py
@@ -1,3 +1,78 @@
 """
 Archivo principal del proyecto downtube.
+Este archivo se encarga de descargar un video de YouTube
+
+Usa la librería pytubefix para descargar videos de YouTube.
+Para instalar pytubefix, ejecuta el siguiente comando:
+pip install pytubefix
+
+Para instalar la librería rich, ejecuta el siguiente comando:
+pip install rich
+
+Para instalar la librería pyfiglet, ejecuta el siguiente comando:
+pip install pyfiglet
+
+Para visualiza los tipos de letra de Figlet, puedes visitar el siguiente enlace:
+https://www.askapache.com/online-tools/figlet-ascii/
+
 """
+import os
+from rich import print as rprint
+
+from pytubefix import YouTube, Playlist
+from pytubefix.cli import on_progress
+
+import pyfiglet 
+
+def pulsa_tecla():
+    input("\n\nPulsa una tecla para continuar...")
+
+while True:
+    os.system("cls")
+
+    texto_formateado = pyfiglet.figlet_format("DownTube", font="slant")
+    rprint(f"[bold magenta]{texto_formateado}[/bold magenta]")
+    rprint(f"[bold magenta]DESCARGADOR DE VIDEOS DE YOUTUBE[/bold magenta]")
+    rprint(f"[bold magenta]Creado con 💗 por @juanfrabrv[/bold magenta]")
+    rprint(f"\n")
+    rprint(f"1 : Descargar vídeo")
+    rprint(f"2 : Descargar audio")
+    rprint(f"3 : Descargar lista de reproducción completa")
+    rprint(f"0 : Salir\n")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "0":
+        break
+    elif opcion == "1":
+        url = input("Introduce la URL del vídeo: ")
+        yt = YouTube(url, on_progress_callback=on_progress)
+        ys = yt.streams.get_highest_resolution()
+        rprint(f"Descargando... {yt.title}")
+
+        ys.download()
+        rprint("[bold green]\nDescarga completada[/bold green]")
+        pulsa_tecla()
+    elif opcion == "2":
+        url = input("Introduce la URL del vídeo ▶ audio: ")
+        yt = YouTube(url, on_progress_callback=on_progress)
+        ys = yt.streams.get_audio_only()
+        rprint(f"Descargando... {yt.title}")
+
+        ys.download()
+        rprint("[bold green]\nDescarga completada[/bold green]")
+        pulsa_tecla()
+        
+    elif opcion == "3":
+        url = input("Introduce la URL del vídeo: ")
+        lista_reproduccion = Playlist(url)
+
+        rprint(f"Descargando lista de reproducción... {lista_reproduccion.title}")
+        rprint(f"Descargando {len(lista_reproduccion.videos)} vídeos")
+        for video in lista_reproduccion.videos:
+            rprint(f"Descargando... {video.title}")
+            ys = video.streams.get_highest_resolution()
+            ys.download()
+
+        rprint("[bold green]\nDescarga completada[/bold green]")
+        pulsa_tecla()
+
